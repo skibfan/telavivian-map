ac6c87c8c14d8cc107f00069e505dd7aae2e655f
kozlozyabr <111320992+skibfan@users.noreply.github.com>
2024-11-13 19:21:30 +0200

    ?number10005

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://telavivian-map.onrender.com',
+  base: './',
   build: {
     outDir: './dist',
     emptyOutDir: true,
