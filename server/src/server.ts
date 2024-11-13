0d37512573e22d2933d684531f7a6ff74402af12
kozlozyabr <111320992+skibfan@users.noreply.github.com>
2024-11-13 17:01:15 +0200

    fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,15 +23,13 @@ app.listen(process.env.PORT || 3001, () => {
   //   res.json({ message: `Hello ${req.params.name}, from server!` });
   // });
   
-  
+  app.use('/api', router)
   // Have Node serve the files for our built React app
   app.use(express.static(path.resolve(__dirname, "./client/build")));
   app.use(express.static(path.join(__dirname, "/client/build")));
   
   // All other GET requests not handled before will return our React app
   app.get("*", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
   
-
-  app.use('/api', router)
